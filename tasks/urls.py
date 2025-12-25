ccc523c63c679f2d1de8ce6ff79ba8e051f024c6
Developer <dev@example.com>
2025-12-25 20:42:51 +0000

    final: Финальный результат - полная CRUD функциональность

---
+++ tasks/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import TaskListView, TaskCreateView, TaskUpdateView, TaskDeleteView
+
+urlpatterns = [
+    path('', TaskListView.as_view(), name='task-list'),
+    path('create/', TaskCreateView.as_view(), name='task-create'),
+    path('<int:pk>/edit/', TaskUpdateView.as_view(), name='task-edit'),
+    path('<int:pk>/delete/', TaskDeleteView.as_view(), name='task-delete'),
+]
--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Task
+from .forms import TaskForm
 
-# Create your views here.
+
+class TaskListView(ListView):
+    model = Task
+    template_name = 'tasks/task_list.html'
+    context_object_name = 'tasks'
+    paginate_by = 10
+
+
+class TaskCreateView(CreateView):
+    model = Task
+    form_class = TaskForm
+    template_name = 'tasks/task_form.html'
+    success_url = reverse_lazy('task-list')
+
+
+class TaskUpdateView(UpdateView):
+    model = Task
+    form_class = TaskForm
+    template_name = 'tasks/task_form.html'
+    success_url = reverse_lazy('task-list')
+
+
+class TaskDeleteView(DeleteView):
+    model = Task
+    template_name = 'tasks/task_confirm_delete.html'
+    success_url = reverse_lazy('task-list')
+
